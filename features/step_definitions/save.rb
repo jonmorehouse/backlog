fe1a9086cd1edd505d74b1575af6b5a85e79701c
Jon Morehouse <MorehouseJ09@Jon-Morehouses-MacBook-Pro.local>
2014-03-29 20:06:33 -0700

    Saturday March 29

--- features/step_definitions/save.rb
+++ features/step_definitions/save.rb
@@ -4,7 +4,6 @@ end
 Then(/^project should be committed to git with proper message$/) do
   command = "cd #{Backlog::Config.base_dir} && git add --all . && git commit -a -m \"#{Date.today.strftime("%A %B %d")}\""
   @exec_spy.has_been_called?.should == true
-  puts command
 
 end
 
